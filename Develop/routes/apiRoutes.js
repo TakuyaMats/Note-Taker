9074da1f53af0913798368dcc0bf3ffd226417ff
TMats <takomats@hotmail.com>
2021-04-20 16:31:15 -0400

    added the delte function in my api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,12 +1,36 @@
-const path = require('path');
+const dataBase = require('../db/db.json');
+const fs = require('fs');
 
 module.exports = (app) => {
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    app.get('/api/notes', (req, res) => res.json(dataBase));
+
+    app.post('/api/notes', (req, res) => {
+        fs.readFile(`${__dirname}/notes.html`, (err, data) => {
+            if (err) throw err;
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+
+        return res.json(false)
     });
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-}
+    app.delete('/api/notes/:id', (req, res) => {
+        const noteId = JSON.parse(req.params.id)
+        console.log(noteId)
+        fs.unlink(`${__dirname}/notes.html`, (err, notes) => {
+            if (err) throw err;
+
+            notes = JSON.parse(notes);
+            notes = notes.filter(value => value.id !== noteId)
+        
+            fs.writeFile(`${__dirname}/notes.html`, JSON.stringify(notes), (err, data) => {
+                if (err) throw err;
+                res.json(notes)
+            })
+        })
+    })
+};
+
+
