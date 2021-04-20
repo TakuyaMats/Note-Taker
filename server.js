2a09a75ae46975106535db13ea19f30615315c2e
TMats <takomats@hotmail.com>
2021-04-20 19:53:11 -0400

    added express static and fixed my folder structure

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-require('../../../routes/apiRoutes')(app);
-require('../../../routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
