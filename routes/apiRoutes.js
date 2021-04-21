85468958abc9ac4b30f1c95f8be047f5472ec436
TMats <takomats@hotmail.com>
2021-04-21 13:45:59 -0400

    finalized my apirouts file and finished my readme file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,45 @@
-const dataBase = require('../db/db.json');
 const fs = require('fs');
+const util = require('util');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = (app) => {
 
-    app.get('/api/notes', (req, res) => res.json(dataBase));
-
-    app.post('/api/notes', (req, res) => {
-        fs.readFile(`${__dirname}/notes.html`, (err, data) => {
-            if (err) throw err;
-
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
-
-        return res.json(false)
+    app.get('/api/notes', function (req, res) {
+        readFileAsync('./db/db.json', 'utf8').then(function (data) {
+            notes = [].concat(JSON.parse(data))
+            res.json(notes);
+        })
     });
 
-    app.delete('/api/notes/:id', (req, res) => {
-        const noteId = JSON.parse(req.params.id)
-        console.log(noteId)
-        fs.unlink(`${__dirname}/notes.html`, (err, notes) => {
-            if (err) throw err;
+    app.post('/api/notes', function (req, res) {
+        const note = req.body;
+        readFileAsync('./db/db.json', 'utf8').then(function (data) {
+            const notes = [].concat(JSON.parse(data));
+            note.id = notes.length + 1
+            notes.push(note);
+            return notes
+        }).then(function (notes) {
+            writeFileAsync('./db/db.json', JSON.stringify(notes))
+            res.json(note);
+        })
+    })
 
-            notes = JSON.parse(notes);
-            notes = notes.filter(value => value.id !== noteId)
-        
-            fs.writeFile(`${__dirname}/notes.html`, JSON.stringify(notes), (err, data) => {
-                if (err) throw err;
-                res.json(notes)
-            })
+    app.delete('/api/notes/:id', function (req, res) {
+        const idToDelete = parseInt(req.params.id);
+        readFileAsync("./db/db.json", 'utf8').then(function (data) {
+            const notes = [].concat(JSON.parse(data));
+            const newNotesData = []
+            for (i = 0; i < notes.length; i++) {
+                if (idToDelete !== notes[i].id) {
+                    newNotesData.push(notes[i])
+                }
+            }
+            return newNotesData
+        }).then(function(notes) {
+            writeFileAsync("./db/db.json", JSON.stringify(notes))
+            res.send('Saved Success!')
         })
     })
 };
